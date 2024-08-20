4e0ff6963a3f6caaed0f771d10564a40f46fe207
Michael Wu <68759331+micwu562@users.noreply.github.com>
2024-08-19 21:35:22 -0700

    i dont understand bundlers man

--- src/lib/blinkDetection/loadmodel.ts
+++ src/lib/blinkDetection/loadmodel.ts
@@ -7,7 +7,9 @@ export async function setupModel() {
 
   const faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
     baseOptions: {
-      modelAssetPath: "/model/face_landmarker.task",
+      // modelAssetPath: "/model/face_landmarker.task",
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
       delegate: "GPU",
     },
     runningMode: "VIDEO",
